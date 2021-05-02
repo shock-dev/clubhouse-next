5fa963152ca150342d140e601782ef55704ba1d9
Andrey <shock.work@mail.ru>
2021-05-02 15:37:43 +0300

    prepare EnterNameStep component

---
+++ components/steps/EnterNameStep/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import clsx from 'clsx';
+import { WhiteBlock } from '../../WhiteBlock';
+import { Button } from '../../Button';
+import { StepInfo } from '../../StepInfo';
+
+import styles from './EnterNameStep.module.scss';
+
+export const EnterNameStep = () => {
+  const [inputValue, setInputValue] = React.useState<string>('');
+
+  const nextDisabled = !inputValue;
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div className={styles.block}>
+      <StepInfo
+        icon="/static/man.png"
+        title="What’s your full name?"
+        description="People use real names on Clubhouse :) Tnx!"
+      />
+      <WhiteBlock className={clsx('m-auto', styles.whiteBlock)}>
+        <div className="mb-30">
+          <input
+            onChange={handleChangeInput}
+            value={inputValue}
+            className="field"
+            placeholder="Enter fullname"
+          />
+        </div>
+        <Button disabled={nextDisabled}>
+          Next
+          <img className="d-ib ml-10" src="/static/arrow.svg" />
+        </Button>
+      </WhiteBlock>
+    </div>
+  );
+};
