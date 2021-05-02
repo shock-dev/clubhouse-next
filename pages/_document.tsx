44b9f4b0f513404fdb421aa60818c80831c90b84
Andrey <shock.work@mail.ru>
2021-05-02 11:28:05 +0300

    transfer all files to .tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, {
+  Html,
+  Main,
+  NextScript,
+  Head,
+  DocumentContext
+} from 'next/document'
+
+class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+          <meta name="viewport" content="viewport-fit=cover" />
+          <title>Clubhouse: Drop-in audio chat</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default AppDocument
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
-import Head from 'next/head';
 import { WelcomeStep } from '../components/steps/WelcomeStep';
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Home page</title>
-      </Head>
       <WelcomeStep />
     </div>
   )
