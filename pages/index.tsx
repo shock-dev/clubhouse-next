38abf3e4e9289536804d2d02ba57ae104bfc72dc
Andrey <shock.work@mail.ru>
2021-05-20 11:53:38 +0300

    prepare all components for outputting in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { EnterNameStep } from '../components/steps/EnterNameStep';
+import { EnterPhoneStep } from '../components/steps/EnterPhoneStep';
+import { ChooseAvatarStep } from '../components/steps/ChooseAvatarStep';
+import { TwitterStep } from '../components/steps/TwitterStep';
+import { WelcomeStep } from '../components/steps/WelcomeStep';
+import { EnterCodeStep } from '../components/steps/EnterCodeStep';
+
+const stepsComponents = {
+  0: WelcomeStep,
+  1: EnterNameStep,
+  2: TwitterStep,
+  3: ChooseAvatarStep,
+  4: EnterPhoneStep,
+  5: EnterCodeStep
+};
+
 
 const Home = () => {
+  const Current = stepsComponents[0];
+
   return (
     <div>
-      <EnterNameStep />
+      <Current />
     </div>
   );
 };
