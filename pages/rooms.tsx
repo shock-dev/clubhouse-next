84b0ff15f4b536ac9475445a13a4383a7f8795b2
Andrey <shock.work@mail.ru>
2021-05-20 12:00:25 +0300

    sketch rooms template

---
+++ pages/rooms.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../components/Header';
+
+export default function RoomsPage() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>ТУТ БУДУТ ВСЕ СТРАНИЦЫ</h1>
+      </div>
+    </>
+  );
+}
