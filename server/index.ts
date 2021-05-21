4a05c00d61cab34b7f18b1d4a87d65f105e70782
Andrey <shock.work@mail.ru>
2021-05-21 20:35:17 +0300

    try to upload image on backend

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,11 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import passport from 'passport';
+import sharp from 'sharp';
+import fs from 'fs';
 import consola from 'consola';
 import connect from './core/db';
+import { uploader } from './core/uploader';
 
 dotenv.config({
   path: 'server/.env'
@@ -21,6 +24,25 @@ app.use(cors());
 app.use(passport.initialize());
 
 app.use('/auth', authRoutes);
+
+app.post('/upload', uploader.single('photo'), (req, res) => {
+  sharp.cache(false);
+  const filePath = req.file.path;
+  sharp(filePath)
+    .resize(150, 150)
+    .toFormat('jpeg')
+    .toFile(filePath.replace('.png', '.jpeg'), (err) => {
+      if (err) {
+        throw err;
+      }
+
+      fs.unlinkSync(filePath);
+
+      res.json({
+        url: `/avatars/${req.file.filename.replace('.png', '.jpeg')}`
+      });
+    });
+});
 
 connect()
   .then(() => {
