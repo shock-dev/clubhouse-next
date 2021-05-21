77229fdf136c031298d63253f0dcbaabc35273d5
Andrey <shock.work@mail.ru>
2021-05-21 20:33:48 +0300

    cancel `withCredentials` in axios

--- core/axios.ts
+++ core/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
-  withCredentials: true
+  baseURL: 'http://localhost:5000'
 });
 
 export default instance;
