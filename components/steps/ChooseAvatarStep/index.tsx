b2087a2d48bb40dea0fe46b9dba894b69684657c
Andrey <shock.work@mail.ru>
2021-05-21 20:35:57 +0300

    try to upload avatar on frontend

--- components/steps/ChooseAvatarStep/index.tsx
+++ components/steps/ChooseAvatarStep/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import { WhiteBlock } from '../../WhiteBlock';
 import { Button } from '../../Button';
@@ -7,19 +7,41 @@ import { Avatar } from '../../Avatar';
 
 import styles from './ChooseAvatarStep.module.scss';
 import { MainContext } from '../../../pages';
+import axios from '../../../core/axios';
+
+const uploadFile = async (file: File): Promise<{ url: string }> => {
+  const formData = new FormData();
+  formData.append('photo', file);
+
+  const { data } = await axios({
+    method: 'POST',
+    url: '/upload',
+    data: formData,
+    headers: { 'Content-Type': 'multipart/form-data' }
+  });
+
+  return data;
+};
 
 export const ChooseAvatarStep: React.FC = () => {
-  const { onNextStep } = useContext(MainContext);
-  const [avatarUrl, setAvatarUrl] = React.useState<string>(
-    'https://sun9-15.userapi.com/impg/PmnLAr-xRXSzloN4DxAqPK0LQVwahjxK6JoCIg/lFaTr4xGyBg.jpg?size=744x619&quality=96&sign=7edeeb31fa3ea0039b71a35551a3e693&type=album'
-  );
+  const { onNextStep, setFieldValue, userData } = React.useContext(MainContext);
+  const avatarLetters = userData.fullname
+    .split(' ')
+    .map((s) => s[0])
+    .join('');
+  const [avatarUrl, setAvatarUrl] = React.useState<string>(userData.avatarUrl);
   const inputFileRef = React.useRef<HTMLInputElement>(null);
 
-  const handleChangeImage = (event: Event): void => {
-    const file = (event.target as HTMLInputElement).files[0];
+  const handleChangeImage = async (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    const file = target.files[0];
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       setAvatarUrl(imageUrl);
+      const data = await uploadFile(file);
+      target.value = '';
+      setAvatarUrl(data.url);
+      setFieldValue('avatarUrl', data.url);
     }
   };
 
@@ -33,12 +55,12 @@ export const ChooseAvatarStep: React.FC = () => {
     <div className={styles.block}>
       <StepInfo
         icon="/static/celebration.png"
-        title="Okay, Andrey Shock!"
+        title={`Okay, ${userData?.fullname}!`}
         description="How’s this photo?"
       />
       <WhiteBlock className={clsx('m-auto mt-40', styles.whiteBlock)}>
         <div className={styles.avatar}>
-          <Avatar width="120px" height="120px" src={avatarUrl} />
+          <Avatar width="120px" height="120px" src={avatarUrl} letters={avatarLetters} />
         </div>
         <div className="mb-30">
           <label htmlFor="image" className="link cup">
@@ -48,7 +70,7 @@ export const ChooseAvatarStep: React.FC = () => {
         <input id="image" ref={inputFileRef} type="file" hidden />
         <Button onClick={onNextStep}>
           Next
-          <img className="d-ib ml-10" src="/static/arrow.svg" alt="Arrow" />
+          <img className="d-ib ml-10" src="/static/arrow.svg" />
         </Button>
       </WhiteBlock>
     </div>
